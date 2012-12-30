a5637d792c22c06577493bee390915a96c9f79f9
Ph3nol <ph3@slynett.com>
2012-12-30 15:49:50 +0100

    Basic example added

---
+++ example/index.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use GitElephant\Repository;
+use Sly\BlogHub\Blog\Blog;
+
+$repo = new Repository('../example/content');
+$repo->checkout('master');
+
+$blog = new Blog($repo);
+
+?>
+
+<h2>Statistics</h2>
+
+<ul>
+    <li>Total categories: <strong><?php echo $blog->getCategories()->count() ?></strong></li>
+    <li>Total posts: <strong><?php echo $blog->getPosts()->count() ?></strong></li>
+    <li>Total tags: <strong><?php echo $blog->getTags()->count() ?></strong></li>
+</ul>
+
+<h2>Categories and Posts</h2>
+
+<ul>
+    <?php foreach ($blog->getCategories() as $c): ?>
+        <li>
+            <strong><?php echo $c->getName() ?></strong> (<?php echo $c->getPosts()->count() ?> posts)
+
+            <ul>
+                <?php foreach ($c->getPosts() as $p): ?>
+                    <li>
+                        <strong><?php echo $p->getCreatedAt()->format('Y-m-d') ?></strong>
+                        <?php echo $p->getTitle() ?>
+                        <br />
+                        <small><?php echo $p->getExcerpt() ?></small>
+
+                        <?php if ($p->getTags()): ?>
+                            <ul>
+                                <?php foreach ($p->getTags() as $tag): ?>
+                                    <li><?php echo $tag ?></li>
+                                <?php endforeach ?>
+                            </ul>
+                        <?php endif ?>
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </li>
+    <?php endforeach ?> 
+</ul>
